69f783b16df66d16effe8de93ccaa00a6ecc494a
Vinicioscastro <viniciospcastro19@gmail.com>
2021-12-10 18:52:30 -0300

    hello world

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,27 @@
 console.log("HEllo World")
+
+var nome = "vinicios"
+
+if (nome == "vinicios") {
+    console.log("1")
+    console.log(nome)
+    var nome2 = "iker"
+    console.log(nome2)
+}
+
+console.log(nome)
+
+console.log(nome2)
+
+for(let i=0; i<10; i++){
+    console.log(i)
+}
+
+var lista = ["carro", "motto", "Bicicleta"]
+console.log(lista)
+
+lista.forEach(elemento => {
+    if (elemento == "Bicicleta"){
+        console.log("tem biscicleta aqui")
+    }
+});
