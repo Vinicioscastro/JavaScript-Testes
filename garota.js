6572389da0d60c0ee4e2f1a08b8fd12c5bb28f37
vinicioscastro <viniciospcastro19@gmail.com>
2021-12-21 00:24:11 -0300

    ensinando a manu

--- garota.js
+++ garota.js
@@ -1,12 +1,50 @@
-a = 8
-b = 27
+// a = 8
+// b = 27
 
-soma = a + b
-subtrair = b - a
-multiplicar = a * b 
-dividir = b / a
+// soma = a + b
+// subtrair = b - a
+// multiplicar = a * b 
+// dividir = b / a
+// // 5/2 = 2 e sobra 1.
+// // 8 / 3 = 2
 
-console.log (subtrair)
-console.log (soma)
-console. log (multiplicar)
-console.log (dividir)
+// c = 5
+// d = 2
+
+// resto = c % d
+
+// console.log (subtrair)
+// console.log (soma)
+// console. log (multiplicar)
+// console.log (dividir)
+
+// if = se
+// else = senao
+
+// parOuImpa = 11
+
+// if ( parOuImpa % 2 == 0){
+//     console.log("Numero par");
+// } else{
+//     console.log("Numero Impa");
+// }
+
+// nota = 7
+
+// if(nota < 7 ){
+//     console.log("Reprovado");
+// }
+// else{
+//     console.log("Aprovado");
+// }
+
+masOuFem = "F"
+
+if (masOuFem == "M"){
+    console.log("Masculino");
+}
+
+if(masOuFem == "F"){
+    console.log("Feminino");
+}
+
