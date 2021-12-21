4eddb6ad99b695f02ee7370b7e5072f1016e248a
vinicioscastro <viniciospcastro19@gmail.com>
2021-12-21 00:06:47 -0300

    ensinando a manu

---
+++ garota.js
@@ -0,0 +1,12 @@
+a = 8
+b = 27
+
+soma = a + b
+subtrair = b - a
+multiplicar = a * b 
+dividir = b / a
+
+console.log (subtrair)
+console.log (soma)
+console. log (multiplicar)
+console.log (dividir)
