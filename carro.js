fe7f2397a2b439303cca0f29c2fd594e078c3691
vinicioscastro <viniciospcastro19@gmail.com>
2021-12-22 06:37:29 -0300

    object

--- carro.js
+++ carro.js
@@ -1,4 +1,19 @@
-class carro {
-    var nome
-    
+class Carro {
+    constructor(nome, valor){
+        this.nome = nome
+        this.valor = valor
+    }
+
+    mostrar(){
+        console.log(`O ${this.nome} hoje custa em media ${this.valor} em nossa loja`);
+    }
 }
+
+var carro1 = new Carro
+
+carro1.nome = "creta"
+carro1.valor = 95.000,00
+
+console.log(carro1);
+carro1.mostrar()
+
