096f9b56a10693c7a504c04c7f3caffbfd7c38d5
vinicioscastro <viniciospcastro19@gmail.com>
2022-01-20 22:32:14 -0300

    atualização

--- manu.js
+++ manu.js
@@ -1,21 +1,21 @@
-// aluguel = 150
-// energia = 60
-// internet = 17.50
-// agua = 15
-// cartao = 100
-// riachuelo = 43.90
+aluguel = 150
+energia = 60
+internet = 17.50
+agua = 15
+cartao = 100
+riachuelo = 43.90
 
-// total = aluguel + energia + agua + internet + cartao + riachuelo
+total = aluguel + energia + agua + internet + cartao + riachuelo
 
-// recebido = 450
+recebido = 450
 
-// console.log("Valor das contass do mÊs: "+total)
+console.log("Valor das contass do mÊs: "+total)
 
-// console.log("Valor que eu recebi no mês: "+recebido)
+console.log("Valor que eu recebi no mês: "+recebido)
 
-// sobra = recebido - total
+sobra = recebido - total
 
-// console.log("Sobrou pra mim: "+ sobra)
+console.log("Sobrou pra mim: "+ sobra)
 
 for (i = 0; i<1000; i++){
     console.log(i);
